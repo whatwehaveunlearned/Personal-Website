815836f6f4772475bb48707b863c012b156aa0cb
agon <agon@hawaii.edu>
2014-07-22 16:36:26 -1000

    actual state

--- files/main.js
+++ files/main.js
@@ -1,17 +1,61 @@
-$(function () {
-	
-	$(window).load(function(){
-		$( "#accordion" ).hide();
-	});
+var count = 0;
+$( "#robotics" ).show();
+$('#higgs' ).hide();
+$('#neuron' ).hide();
+$('#gazebo' ).hide();
+$('#gazebo' ).hide();
+$( "#accordion" ).hide();
 
-	$( "#homeBtn" ).click(function() {
+$( "#homeBtn" ).click(function() {
 		$( "#accordion" ).hide();
-		$('#intro' ).show();
-  		$('#myCarousel' ).show();
+  		$('#intro' ).show();
 	});
   	$( "#ProjectBtn" ).click(function() {
-  	$('#myCarousel' ).hide();
-  	$('#intro' ).hide();
-  	$( "#accordion" ).show();
+	  	$('#intro' ).hide();
+	  	$( "#accordion" ).show();
 	});
-});
+
+//main function
+setInterval(function main() {
+
+  	count=changeImage(count);
+  	},10000);
+	
+	function changeImage(count){
+		switch(count) {
+		    case 0:
+		    	$( "#robotics" ).hide();
+				$('#higgs' ).hide();
+				$('#neuron' ).hide();
+				$('#gazebo' ).show();
+				count = count+1;
+		        break;
+		    case 1:
+		    	count = count+1;
+		       	$( "#robotics" ).hide();
+				$('#higgs' ).show();
+	  			$('#neuron' ).hide();
+	  			$('#gazebo' ).hide();
+		        break;
+		     case 2:
+		     	count = count+1;
+		    	$( "#robotics" ).hide();
+				$('#higgs' ).hide();
+	  			$('#neuron' ).show();
+	  			$('#gazebo' ).hide();
+	  			count = count+1;
+		        break;
+	        case 3:
+				count = count+1;
+		        $( "#robotics" ).show();
+				$('#higgs' ).hide();
+	  			$('#neuron' ).hide();
+	  			$('#gazebo' ).hide();
+	        break;
+
+		    default:
+		    	count = 0;
+		}
+
+		return(count);
+	}
